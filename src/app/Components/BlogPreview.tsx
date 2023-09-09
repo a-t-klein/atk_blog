ecadf37308ce41988c1465dc3549b51f35777a0a
a-t-klein <alxtkln@gmail.com>
2023-09-08 19:49:13 -0700

    initial blog preview layout with dummy data

--- src/app/Components/BlogPreview.tsx
+++ src/app/Components/BlogPreview.tsx
@@ -11,12 +11,12 @@ const BlogPreview: React.FC<BlogPostProps> = ({
   tags,
 }) => {
   return (
-    <div key={id} className="grid md:grid-cols-2 sm:grid-cols-1">
-      <div className="w-full relative">
-        <Image src={img} alt={altTxt} width={300} height={150} />
+    <div key={id} className="grid md:grid-cols-2 sm:grid-cols-1 gap-6">
+      <div className="w-full h-80 relative">
+        <Image src={img} alt={altTxt} fill={true} />
       </div>
       <div>
-        <h1>{title}</h1>
+        <h1 className="">{title}</h1>
         <p>{description}</p>
         <span>
           {tags.map((tag) => (
--- src/app/Containers/BlogContainer.tsx
+++ src/app/Containers/BlogContainer.tsx
@@ -11,7 +11,7 @@ const BlogContainer = () => {
         altTxt="img"
         title="new blog!"
         text=""
-        description="blog post"
+        description="perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni..."
         tags={["cool", "first"]}
       />
       <div>loading :)</div>
